cdf962fd9e626269047e4a7097271a8d87424030
fredbarros <fredb52@gmail.com>
2013-05-06 23:47:32 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bemestar::Application.config.secret_token = '5e8a4629fda641e023079513fe5edaff486333a5cd88834cd6cc5d386c56257abc1261dc922ed7f5c0c94aeea4518340bea08a76c30d1c0ae6e9813b4f0779a0'
